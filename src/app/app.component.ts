d4ee7f2972258f816ea48ef2b9e2371c1d7ef8e7
Tyrone Showers <info@taliferro.com>
2026-09-11 22:07:50 -0700

    menu, sounds updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { EmailSignatureBuilderComponent } from './signature-builder/email-signature-builder.component';
 import { CommandPaletteComponent } from './shared/page/command-palette/command-palette.component';
 import { SiteFooterComponent } from './shared/site-footer/site-footer.component';
+import { PlatformMenuComponent } from './shared/platform-menu/platform-menu.component';
 
 @Component({
   selector: 'app-root',
-  imports: [EmailSignatureBuilderComponent, CommandPaletteComponent, SiteFooterComponent],
+  imports: [EmailSignatureBuilderComponent, CommandPaletteComponent, SiteFooterComponent, PlatformMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
